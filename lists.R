bbbf0b57a2bf9625d4a176e0848447cbbce32f8c
achew8 <70230739+achew8@users.noreply.github.com>
2020-10-22 01:32:51 -0700

    Add files via upload

---
+++ lists.R
@@ -0,0 +1,10 @@
+r <- list()
+for (i in 1:10) {  r[[i]] <- sample(1:6,i,replace=TRUE) 
+} 
+for (i in r) {
+	cat(i,"\n")
+	}
+rsum <- sapply(r,sum) 
+cat("sums=",rsum,"\n") 
+rmean <- sapply(r,mean) 
+cat("means=",rmean,"\n") 
